5b3201d08b9eba4c356f403ce93e7bd7cbd93975
Dennis Demir <dd222gc@student.lnu.se>
2024-06-27 16:31:43 +0200

    Adding undo move button

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { useGameContext } from '../context/GameContext'
+import History from './History'
 
 const Header = () => {
   const { currentPlayer } = useGameContext()
@@ -9,6 +10,9 @@ const Header = () => {
     <div className='text-center my-4'>
       <h1 className='text-3xl font-bold'>Connect 4</h1>
       <h2 className='text-xl mt-2'>Current Player: {currentPlayer}</h2>
+      <div>
+        <History />
+      </div>
     </div>
   )
 }
